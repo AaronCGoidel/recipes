97a94ab532687d30a04f5f11055e2761972a438c
AaronCGoidel <agoidel2019@mka.org>
2018-01-24 12:35:46 -0500

    added postgres link

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 module.exports = {
 				express: {
 								port: 8081
+				},
+				data: {
+								URL: "postgres://sovskiacufbhae:433c00f503b327289b464fd98ea4b491c1d00a6030e3500f5eba27dc5883550e@ec2-54-243-193-227.compute-1.amazonaws.com:5432/d65h5rsh047b11"
 				}
 };
