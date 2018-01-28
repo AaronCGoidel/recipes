b72a232ebd21ff031120b4e8f5ec3c1178052119
AaronCGoidel <agoidel2019@mka.org>
2018-01-27 22:44:59 -0500

    cleaned up app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const router = express.Router();
 var pg = require('pg');
@@ -25,7 +25,7 @@ const CLIENT_ID = process.env.CLIENT_ID || config.google.client_id;
 const PORT = process.env.PORT || config.express.port;
 
 // Use pug
-app.set("view engine", "pug");
+app.set('view engine', 'pug');
 
 // Setup OAuth
 var auth = new GoogleAuth;
@@ -34,124 +34,118 @@ var client = new auth.OAuth2(CLIENT_ID, '', '');
 router.use(cookieParser());
 
 // test database connection
-db.sequelize
-				.authenticate()
-				.then(() => {
-								console.log('[DATABASE]Connection has been established successfully.');
-				})
-				.catch(err => {
-								console.error('[DATABASE]Unable to connect to the database:', err);
-				});
+db.sequelize.authenticate().then(() => {
+  console.log('[DATABASE]Connection has been established successfully.');
+}).catch(err => {
+  console.error('[DATABASE]Unable to connect to the database:', err);
+});
 
 // Print request methods
-router.use(function (req, res, next) {
-				console.log("/" + req.method);
-				next();
+router.use(function(req, res, next) {
+  console.log('/' + req.method);
+  next();
 });
 
 //==========Endpoints==========//
 
 // homepage
-router.get("/", authMiddleware, async function (req, res) {
-				// query for all recipes from newest to oldest
-				const recipeQuery = db.Recipe.findAll({
-								where: {
-												author: req.cookies.uid
-								},
-								order: [['createdAt', 'DESC']]
-				});
+router.get('/', authMiddleware, async function(req, res) {
+  // query for all recipes from newest to oldest
+  const recipeQuery = db.Recipe.findAll({
+    where: {
+      author: req.cookies.uid,
+    },
+    order: [['createdAt', 'DESC']],
+  });
 
-				const userQuery = db.User.findAll({
-								where: {
-												id: req.cookies.uid
-								}
-				});
+  const userQuery = db.User.findAll({
+    where: {
+      id: req.cookies.uid,
+    },
+  });
 
-				const recipes = await recipeQuery;
-				const user = await userQuery;
+  const recipes = await recipeQuery;
+  const user = await userQuery;
 
-				// render homepage with recipes
-				res.render("index", {user, recipes});
+  // render homepage with recipes
+  res.render('index', {user, recipes});
 });
 
+router.get('/recipe/*', function(req, res) {
+  // get recipe id from url
+  var getID = /[^/]*$/.exec(req.path)[0];
+  console.log(getID);
 
-router.get("/recipe/*", function (req, res) {
-				// get recipe id from url
-				var getID = /[^/]*$/.exec(req.path)[0];
-				console.log(getID);
-
-				// query database for recipe with id from url
-				db.Recipe.findOne({
-								where: {
-												id: getID
-								}
-				}).then(thisRecipe => res.render("recipe", {thisRecipe}));
+  // query database for recipe with id from url
+  db.Recipe.findOne({
+    where: {
+      id: getID,
+    },
+  }).then(thisRecipe => res.render('recipe', {thisRecipe}));
 });
 
 // Authentication middleware
 function authMiddleware(req, res, next) {
-				if (req.cookies.uid) {
-								next();
-				} else {
-								res.status(401).redirect("/login").end();
-				}
+  if (req.cookies.uid) {
+    next();
+  } else {
+    res.status(401).redirect('/login').end();
+  }
 }
 
-router.get("/login", function (req, res) {
-				res.render("login")
+router.get('/login', function(req, res) {
+  res.render('login');
 });
 
 function verify(token, client_id) {
-				return new Promise((resolve, reject) => {
-								payload = null;
-								client.verifyIdToken(token, client_id, function (e, login) {
-												if (e) {
-																reject(e);
-												} else {
-																payload = login.getPayload();
-																resolve(payload);
-												}
-								})
-				}).catch(function (err) {
-								console.log('there was an error');
-				});
+  return new Promise((resolve, reject) => {
+    payload = null;
+    client.verifyIdToken(token, client_id, function(e, login) {
+      if (e) {
+        reject(e);
+      } else {
+        payload = login.getPayload();
+        resolve(payload);
+      }
+    });
+  }).catch(function(err) {
+    console.log('there was an error');
+  });
 }
 
 // Handle OAuth login POST
-router.post("/auth", function (req, res) {
-				verify(req.body.idtoken, CLIENT_ID).then(function (user) {
-								db.User
-												.findOrCreate({
-																where: {
-																				id: user.sub
-																}, defaults: {
-																				name_first: user.given_name,
-																				name_last: user.family_name
-																}
-												})
-												.spread((user, created) => {
-																console.log(user.get({
-																				plain: true
-																}));
-																console.log(created);
-												});
-								res.cookie("uid", user.sub);
-								res.json(user);
-				});
+router.post('/auth', function(req, res) {
+  verify(req.body.idtoken, CLIENT_ID).then(function(user) {
+    db.User.findOrCreate({
+      where: {
+        id: user.sub,
+      }, defaults: {
+        name_first: user.given_name,
+        name_last: user.family_name,
+      },
+    }).spread((user, created) => {
+      console.log(user.get({
+        plain: true,
+      }));
+      console.log(created);
+    });
+    res.cookie('uid', user.sub);
+    res.json(user);
+  });
 });
 
-router.post("/deauth", authMiddleware, function (req, res) {
-				res.clearCookie("uid");
-				res.send();
+router.post('/deauth', authMiddleware, function(req, res) {
+  res.clearCookie('uid');
+  res.send();
 });
 
-app.use("/", router);
+app.use('/', router);
 
-app.use("*", function (req, res) {
-				res.render("404");
+app.use('*', function(req, res) {
+  res.render('404');
 });
 
-app.listen(PORT, function () {
-				console.log("Listening on Port " + PORT);
+app.listen(PORT, function() {
+  console.log('Listening on Port ' + PORT);
 });
 
