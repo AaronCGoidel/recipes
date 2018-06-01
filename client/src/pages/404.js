2aabc21eb0f1b55f8ef869f10b981f684d6a1e35
AaronCGoidel <agoidel2019@mka.org>
2018-06-01 11:06:43 -0400

    make 404 basic

--- client/src/pages/404.js
+++ client/src/pages/404.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import {Link} from 'react-router-dom';
+
+import '../index.css'
 
 class FourPage extends React.Component {
   render () {
     return (
-        <div>
-          <Typography style={{
-            color: '#ffffff',
-            padding: '0 10px 8px'
-          }} variant="display3">
-            Oopsie Whoopsie!!
+        <div className={'columnar'} style={{background: '#ffffff'}}>
+          <Typography variant={'display1'}>
+            404
           </Typography>
-          <Typography style={{
-            color: '#ffffff',
-            padding: '0 10px 8px'
-          }} variant={"body2"}>
-            uWu
-          </Typography>
-          <Typography style={{
-            color: '#ffffff',
-            padding: '0 10px 8px'
-          }} variant="body2">
-            we made a fucky wucky!!1 a wittle fucko boingo! <br/>
-            the code monkies at our headquarters are working VEWY HAWD to fix this!
-          </Typography>
+        <Link style={{
+            "width": '100px',
+            "fontFamily": "Roboto",
+            "display": 'block',
+            "padding": '15px 35px',
+            "background":'transparent',
+            "border": '2px solid #E73C7E',
+            "borderRadius": '30px',
+            "fontSize": '1em',
+            "fontWeight": '500',
+            "color": '#E73C7E',
+            "transition": '0.3s ease-in-out',
+            "cursor": 'pointer',
+            "outline": 0,
+            "textDecoration": 'none'
+          }} to={'/'}>Go Home</Link>
         </div>
     )
   }
